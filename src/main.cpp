5568eda989252f41947863fe0b5528316cbd09fe
Emilio Guevara <emiliomercadoguevara@gmail.com>
2026-08-05 21:28:31 +0800

    fix: remove legacy SKSEPlugin_Query export for modern SKSE 2.2.x compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ struct SKSEPluginVersionData
     char author[256]{ "Darkraddish" };
     uint32_t addressLibraryVersion{ 1 };
     uint32_t sigStructVersion{ 1 };
-    uint32_t compatibleVersions[16]{ 0x01064920, 0x01060930, 0x01050610, 0 };
+    uint32_t compatibleVersions[16]{ 0 };
     uint32_t minimumSKSEVersion{ 0 };
 };
 
@@ -22,30 +22,11 @@ extern "C" __declspec(dllexport) constinit SKSEPluginVersionData SKSEPlugin_Vers
     1,                           // Plugin Version
     "Cardinal Weapon Framework", // Name
     "Darkraddish",                // Author
-    1,                           // Address Library Version (1 = Uses Address Library / AE compatible)
+    1,                           // Address Library Version (1 = AE / Address Library compatible)
     1,                           // Signature Struct Version (1 = Uses Updated Structs)
-    { 0x01064920, 0x01060930, 0x01050610, 0 }, // Compatible Versions (Skyrim AE 1.6.1170, 1.6.640, 1.5.97)
+    { 0 },                       // 0 = Compatible with all Address Library versions
     0                            // Minimum SKSE Version
 };
-
-// SKSE Plugin Query Info Export
-struct SKSEPluginInfo
-{
-    uint32_t infoVersion{ 1 };
-    const char* name{ "Cardinal Weapon Framework" };
-    uint32_t version{ 1 };
-};
-
-extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Query(const void* a_skse, SKSEPluginInfo* a_info)
-{
-    if (a_info)
-    {
-        a_info->infoVersion = 1;
-        a_info->name = "Cardinal Weapon Framework";
-        a_info->version = 1;
-    }
-    return true;
-}
 
 // SKSE Plugin Load Entry Point Export
 extern "C" __declspec(dllexport) bool SKSEAPI SKSEPlugin_Load(const void* a_skse)

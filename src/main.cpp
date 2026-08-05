ecc99e2563d527ae035ec2bec0747d183de898b8
Emilio Guevara <emiliomercadoguevara@gmail.com>
2026-08-05 21:31:15 +0800

    chore: update framework and release package version to v0.1.0 (Version 0, Build 1)

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@
 struct SKSEPluginVersionData
 {
     uint32_t dataVersion{ 1 };
-    uint32_t pluginVersion{ 1 };
-    char name[256]{ "Cardinal Weapon Framework" };
+    uint32_t pluginVersion{ 1 }; // Version 0, Build 1
+    char name[256]{ "Cardinal Weapon Framework v0.1.0" };
     char author[256]{ "Darkraddish" };
     uint32_t addressLibraryVersion{ 1 };
     uint32_t sigStructVersion{ 1 };
@@ -18,14 +18,14 @@ struct SKSEPluginVersionData
 };
 
 extern "C" __declspec(dllexport) constinit SKSEPluginVersionData SKSEPlugin_Version = {
-    1,                           // Data Version
-    1,                           // Plugin Version
-    "Cardinal Weapon Framework", // Name
-    "Darkraddish",                // Author
-    1,                           // Address Library Version (1 = AE / Address Library compatible)
-    1,                           // Signature Struct Version (1 = Uses Updated Structs)
-    { 0 },                       // 0 = Compatible with all Address Library versions
-    0                            // Minimum SKSE Version
+    1,                                  // Data Version
+    1,                                  // Plugin Version (Version 0, Build 1)
+    "Cardinal Weapon Framework v0.1.0", // Name
+    "Darkraddish",                       // Author
+    1,                                  // Address Library Version (1 = AE / Address Library compatible)
+    1,                                  // Signature Struct Version (1 = Uses Updated Structs)
+    { 0 },                              // 0 = Compatible with all Address Library versions
+    0                                   // Minimum SKSE Version
 };
 
 // SKSE Plugin Load Entry Point Export
